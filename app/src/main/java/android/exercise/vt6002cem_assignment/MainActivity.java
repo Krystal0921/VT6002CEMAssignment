5bc415461059f3d533495056a7b4c13563be55c6
Krystal <128454503+Krystal0921@users.noreply.github.com>
2025-04-02 18:36:15 +0800

    create project

---
+++ app/src/main/java/android/exercise/vt6002cem_assignment/MainActivity.java
@@ -0,0 +1,15 @@
+package android.exercise.vt6002cem_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.exercise.vt6002cem_assignment.R;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
